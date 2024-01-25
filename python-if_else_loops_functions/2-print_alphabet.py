46f29f5f9138f0c02847c22fc3e06e1d625ff7b2
massygh <massyghen1@gmail.com>
2024-01-25 03:49:40 -0800

    alphabet

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
-for char_code in range(ord('a'), ord('z') + 1):
-    character = chr(char_code)
-    print(f"{character}", end='')
+for i in range(97, 123):
+    print(chr(i), end='')
