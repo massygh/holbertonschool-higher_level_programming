cf3ccfd49570b9a4bb7a58dbf03a4120e7d6534f
massygh <massyghen1@gmail.com>
2024-05-08 18:33:55 +0200

    pow

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def pow(a, b):
-    result = 1
-    for i in range(b):
-        result *= a
+    result = 1                   # Étape 1: Initialisation du résultat à 1
+    for _ in range(abs(b)):     # Étape 2: Itération à travers la valeur absolue de b
+        result *= a             # Étape 3: Calcul de la puissance en multipliant result par a
+    if b < 0:                   # Si b est négatif
+        return 1 / result       # Étape 4: Si b est négatif, on retourne l'inverse du résultat
     return result
+
