8b02bfbe58f656830e805bb707f28f150d6b0508
massygh <massyghen1@gmail.com>
2024-05-24 12:24:14 +0200

    creat folder

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def inherits_from(obj, a_class):
+    """Check if an object is an instance of a class that inherited from
+    the specified class."""
+    return issubclass(type(obj), a_class) and type(obj) != a_class
