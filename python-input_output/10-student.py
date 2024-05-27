5451c67aa3c01391636bf9ca5f79d253a52ede51
massygh <massyghen1@gmail.com>
2024-05-27 15:49:29 +0200

    json

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+"""Module to create a class"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of the Student instance.
+
+        Args:
+            attrs (list): A list of attribute names to retrieve.
+
+        Returns:
+            dict: A dictionary representation of the instance with specified attributes.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            filtered_dict = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    filtered_dict[attr] = getattr(self, attr)
+            return filtered_dict
