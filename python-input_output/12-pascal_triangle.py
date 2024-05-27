1292024071e9e0fe54255fb35e86cff6896569b5
massygh <massyghen1@gmail.com>
2024-05-27 16:16:40 +0200

    json

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -2,18 +2,18 @@
 """Module to create a class"""
 
 def pascal_triangle(n):
+    """Returns a list of lists of integers representing the Pascal’s triangle of n"""
     if n <= 0:
         return []
-
-    triangle = []  # List to store the rows of Pascal's triangle
-
-    for i in range(n):
-        row = [1] * (i + 1)  # Initialize the row with 1s
-
+    if n == 1:
+        return [[1]]
+    if n == 2:
+        return [[1], [1, 1]]
+    triangle = [[1], [1, 1]]
+    for i in range(2, n):
+        row = [1]
         for j in range(1, i):
-            # Compute the middle values as the sum of the corresponding values in the previous row
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
         triangle.append(row)
-
     return triangle
