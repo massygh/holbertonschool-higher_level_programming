06585ecc0b371013a8a72991c946271d9bc23543
massygh <massyghen1@gmail.com>
2024-05-27 16:38:08 +0200

    json

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Module to save a json file"""
+# module: 7-add_item.py
+"""Add all arguments to a Python list, and then save them to a file."""
 import sys
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+
+# Importer les fonctions spécifiques depuis les fichiers Python
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 def add_to_list_and_save(args):
     existing_list = load_from_json_file("add_item.json")
