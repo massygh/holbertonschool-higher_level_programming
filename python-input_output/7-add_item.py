1af6a0153de67a340cb9e160ecbdafd8324b6af2
massygh <massyghen1@gmail.com>
2024-05-27 16:39:22 +0200

    json

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,11 +7,8 @@ import sys
 from save_to_json_file import save_to_json_file
 from load_from_json_file import load_from_json_file
 
+
 def add_to_list_and_save(args):
     existing_list = load_from_json_file("add_item.json")
     existing_list.extend(args)
     save_to_json_file(existing_list, "add_item.json")
-
-if __name__ == "__main__":
-    args = sys.argv[1:]
-    add_to_list_and_save(args)
