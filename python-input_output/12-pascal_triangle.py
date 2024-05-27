151ebe6544edc97047d4c49e395cbe42941452b5
massygh <massyghen1@gmail.com>
2024-05-27 16:17:12 +0200

    json

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -2,7 +2,7 @@
 """Module to create a class"""
 
 def pascal_triangle(n):
-    """Returns a list of lists of integers representing the Pascal’s triangle of n"""
+    """Returns a list of lists of integers the Pascal’s triangle of n"""
     if n <= 0:
         return []
     if n == 1:
