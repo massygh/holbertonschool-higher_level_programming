f785b63321f33d4b8f96bf7415063d6b1b9af343
massygh <massyghen1@gmail.com>
2024-05-27 14:57:42 +0200

    json

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import json
+
+def load_from_json_file(filename):
+    """
+    Creates an object from a JSON file.
+
+    Args:
+        filename (str): The name of the JSON file.
+
+    Returns:
+        object: The Python object created from the JSON file.
+    """
+    with open(filename, 'r') as file:
+        return json.load(file)
