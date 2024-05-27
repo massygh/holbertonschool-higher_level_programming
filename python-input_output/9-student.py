6de531446b71b790ae10eb86c086b4023a127c0b
massygh <massyghen1@gmail.com>
2024-05-27 15:45:42 +0200

    json

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -12,12 +12,3 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
-    def to_json(self):
-        """
-        Retrieves a dictionary representation of the Student instance.
-
-        Returns:
-            dict: A dictionary representation of the instance.
-        """
-        return self.__dict__
