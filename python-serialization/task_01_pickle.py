db8173e296bf02d2856d3c4e7e770e82063b94b1
massygh <massyghen1@gmail.com>
2024-05-30 11:38:43 +0200

    json

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,5 +1,5 @@
+#!/usr/bin/python3
 import pickle
-
 
 class CustomObject:
     def __init__(self, name, age, is_student):
@@ -13,47 +13,17 @@ class CustomObject:
         print(f"Is Student: {self.is_student}")
 
     def serialize(self, filename):
-        """
-        Sérialise l'instance actuelle de l'objet et l'enregistre
-        :param filename: Nom du fichier où l'objet sera sérialisé.
-        """
         try:
             with open(filename, 'wb') as file:
                 pickle.dump(self, file)
-            print(f"Object serialized and saved to '{filename}'.")
         except Exception as e:
-            print(f"Failed to serialize object: {e}")
+            print(f"Error during serialization: {e}")
 
     @classmethod
     def deserialize(cls, filename):
-        """
-        Charge et retourne une instance de CustomObject à partir du fichier
-        :param filename: Nom du fichier où l'objet est sérialisé.
-        :return: Instance de CustomObject ou None en cas d'erreur.
-        """
         try:
             with open(filename, 'rb') as file:
-                obj = pickle.load(file)
-            print(f"Object deserialized from '{filename}'.")
-            return obj
-        except (FileNotFoundError, pickle.UnpicklingError) as e:
-            print(f"Failed to deserialize object: {e}")
+                return pickle.load(file)
+        except Exception as e:
+            print(f"Error during deserialization: {e}")
             return None
-
-# Exemple d'utilisation
-
-
-if __name__ == "__main__":
-    # Crée une instance de CustomObject
-    obj = CustomObject(name="John", age=25, is_student=True)
-    print("Original Object:")
-    obj.display()
-
-    # Sérialise l'objet
-    obj.serialize("object.pkl")
-
-    # Désérialise l'objet dans une nouvelle instance
-    new_obj = CustomObject.deserialize("object.pkl")
-    print("\nDeserialized Object:")
-    if new_obj:
-        new_obj.display()
